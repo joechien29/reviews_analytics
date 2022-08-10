cbcb40a00eb191cf8971e759242a4fa45024e79c
joechien29 <76095375+joechien29@users.noreply.github.com>
2022-08-10 21:43:05 +0800

    calc average

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -8,8 +8,15 @@ with open("reviews.txt", "r") as f:
 		if count % 1000 == 0:
 			print(count)
 
-print(len(data))
+print("檔案讀取完了，總共有", len(data), "筆資料")
 
-print(data[0])
-print("----------------------------")
-print(data[1])
+
+reviews_len = 0
+for i in range(0, len(data) - 1):
+	data_len = len(data[i]) # 每筆留言的長度
+	reviews_len = reviews_len + data_len # 加總留言長度
+
+# 平均留言長度
+aver_len = reviews_len / len(data)
+print("平均留言長度為: ", aver_len, "個字母")
+
